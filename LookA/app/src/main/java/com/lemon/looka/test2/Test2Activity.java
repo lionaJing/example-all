edb35823d94f8dfd25cd03eaae95b87d0c807835
JingShuai <xingqiyi312@sina.cn>
2018-07-11 18:35:09 +0800

    mainXML

--- LookA/app/src/main/java/com/lemon/looka/test2/Test2Activity.java
+++ LookA/app/src/main/java/com/lemon/looka/test2/Test2Activity.java
@@ -1,6 +1,15 @@
 package com.lemon.looka.test2;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.lemon.looka.R;
+
 public class Test2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
 }
